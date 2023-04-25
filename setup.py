ae9e4f85629b39c0a47d9c526e05fac8fabe72a6
Matthew Pitkin <m@ttpitk.in>
2023-04-25 07:26:57 +0100

    Move from setup.py to pyproject.toml to specify build information

--- setup.py
+++ setup.py
@@ -35,47 +35,29 @@ Attention: some Cython versions also need the Pyrex module installed!
 import warnings
 import os
 
-try:
-    import setuptools
-except ImportError:
-    warnings.warn("setuptools not found, resorting to distutils: unit test suite can not be run from setup.py")
-    setuptools = None
+from setuptools import setup
+from setuptools import Extension
 
-setup_options = {}
-
-if setuptools is None:
-    from distutils.core import setup
-    from distutils.extension import Extension
-else:
-    from setuptools import setup
-    from setuptools.extension import Extension
-    setup_options['test_suite'] = 'tests'
-    
-   
 import numpy
 
 
+setup_options = {}
+setup_options['test_suite'] = 'tests'
+
+ext_modules = [
+    Extension(
+        "nsgt._nsgtf_loop",
+        sources=["nsgt/nsgtf_loop.pyx"],
+        include_dirs=[numpy.get_include()],
+    ),
+    Extension(
+        "nsgt._nsigtf_loop",
+        sources=["nsgt/nsigtf_loop.pyx"],
+        include_dirs=[numpy.get_include()],
+    ),
+]
+
 setup(
-    name = "nsgt",
-    version = "0.18",
-    author = "Thomas Grill",
-    author_email = "gr@grrrr.org",
-    maintainer = "Thomas Grill",
-    maintainer_email = "gr@grrrr.org",
-    description = "Python implementation of Non-Stationary Gabor Transform (NSGT)",
-    license = "Artistic License",
-    keywords = "fourier gabor",
-    url = "http://grrrr.org/nsgt",
-    requires = ["numpy"],
-    include_dirs = [numpy.get_include()],
-    packages = ['nsgt'],
-    cmdclass = {},
-    ext_modules = [],
-    classifiers = [
-        "Development Status :: 4 - Beta",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "License :: OSI Approved :: Artistic License",
-        "Programming Language :: Python"
-    ],
+    ext_modules = ext_modules,
     **setup_options
 )
